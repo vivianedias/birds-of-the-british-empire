019a4dba51c9ec1d7476ca8970fc633810455744
Viviane Dias <vivianedias@pm.me>
2023-09-16 14:11:08 -0300

    feat: makes prompt to dall-e based on ai response

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv-safe";
 import {
-  Document,
   ContextChatEngine,
   VectorStoreIndex,
   storageContextFromDefaults,
   SimpleDirectoryReader,
 } from "llamaindex";
+import OpenAI from "openai";
 
 dotenv.config();
 
@@ -26,16 +26,29 @@ async function main() {
   const chatEngine = new ContextChatEngine({ retriever });
 
   // Query the index
-  const response = await chatEngine.chat("Describe the readbreast trush", [
-    {
-      content:
-        "Act like Charlotte Bronte, the 19th century English writer, and answer questions SOLELY based on the context provided to you. DO NOT use your previous knowledge. Copy Charlotte Bronte's writing style.",
-      role: "system",
-    },
-  ]);
+  const response = await chatEngine.chat(
+    "Describe the white wagtail appearance. Don't mention its name.",
+    [
+      {
+        content:
+          "You are Charlotte Bronte, the 19th-century English writer. Answer questions SOLELY based on the context provided to you. DO NOT use your previous knowledge. Copy Charlotte Bronte's writing style from her book 'Jane Eyre' and respond like you are providing a prompt to Open AI's DALL-E image generator. Your answers should have a maximum of 1000 characters total.",
+        role: "system",
+      },
+    ]
+  );
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const imageResponse = await openai.images.generate({
+    prompt: response.toString(),
+    n: 3,
+    size: "1024x1024",
+  });
 
   // Output response
   console.log(response.toString());
+  console.log(imageResponse);
 }
 
 main();
